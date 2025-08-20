af71aee175644ba18b971e349cb1ac64c0f7d5cd
Tolu <toludavid30@gmail.com>
2025-08-20 12:32:39 +0100

    footer commit

--- src/layOut Components/Footer.jsx
+++ src/layOut Components/Footer.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
 import "./styling/styling.css"
+import "./styling/mediaQuery.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div id="Footer" className="py-3 d-flex flex-column gap-5">
+      <div className="wrapper container p-4 d-flex justify-content-between">
+        <div className="flowDiv w-25">
+          <h5>
+            <span className='logo'>
+                        <img src="/footer logo.png" alt="Logo" />
+            </span>
+            <span>
+              Creator Flow
+            </span>
+          </h5>
+          <p className="">
+            Creators Flow is the ultimate hub for creators to collaborate, learn, and grow. We help you turn ideas into impact.
+          </p>
+        </div>
+        <div className="quickLinks">
+          <h5>
+            Quick Links 
+          </h5>
+          <ul>
+            <li className=""><a href="" className="text-decoration-none text-light">Home</a></li>
+            <li className=""><a href="" className="text-decoration-none text-light">How it works</a></li>
+            <li className=""><a href="" className="text-decoration-none text-light">Features</a></li>
+            <li className=""><a href="" className="text-decoration-none text-light">Who it's for</a></li>
+            <li className=""><a href="" className="text-decoration-none text-light">Why us</a></li>
+          </ul>
+        </div>
+        <div className="socials">
+          <h5>
+            Social Media 
+          </h5>
+          <ul>
+            <li className=""><a href="" className="text-decoration-none text-light">Instagram</a></li>
+            <li className=""><a href="" className="text-decoration-none text-light">Facebook</a></li>
+            <li className=""><a href="" className="text-decoration-none text-light">X</a></li>
+            <li className=""><a href="" className="text-decoration-none text-light">Tiktok</a></li>
+            <li className=""><a href="" className="text-decoration-none text-light">LinkedIn</a></li>
+          </ul>
+        </div>
+        <div className="connected w-25">
+          <h5>
+            Stay Connected
+          </h5>
+          <p>
+            Join our community – get tips, resources, and updates straight to your inbox.
+          </p>
+          <a href="" className="btn btn-small rounded-5 py-2 px-3 text-light">Subscribe Now</a>
+        </div>
+      </div>
+      <p className="text-center">
+        © 2025 Creators Flow. All rights reserved.
+      </p>
+    </div>
   )
 }
 
